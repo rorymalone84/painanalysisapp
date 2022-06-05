117f6ea22ceb55670d073733e919fbf08dfdcd6b
rory <rorymalone@live.com>
2022-06-05 16:41:56 +0100

    added secure asset to css and js scripts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @routes
-    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
-    <script src="{{ asset(mix('js/app.js')) }}" defer></script>
+    <link href="{{ secure_asset(mix('css/app.css')) }}" rel="stylesheet">
+    <script src="{{ secure_asset(mix('js/app.js')) }}" defer></script>
     @inertiaHead
   </head>
   <body>
